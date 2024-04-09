25e59133205a1976339cc2b500b9e87c7f9a8409
shilpa4103 <shilpashree4103@gmail.com>
2024-04-09 10:25:30 +0530

    peer to peer video conference done

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ let uid = String(Math.floor(Math.random() * 10000));
 let client;
 let channel;
 
+let queryString = window.location.search
+let urlParams = new URLSearchParams(queryString)
+let roomId = urlParams.get('room')
+if(!roomId){
+    window.location = 'lobby.html'
+}
+
 let localStream; // variable will hold the local media stream (camera and microphone).
 let remoteStream; // variable will hold the remote media stream received from another user.
 let peerConnection; // variable represents the WebRTC peer connection between the local and remote users.
@@ -17,22 +24,38 @@ const servers = {
     ]
 };
 
+
+let constraints={
+    video:{
+        width:{min:640,ideal:1920,max:1920},
+        height:{min:400,ideal:1080,max:1080},
+    },
+    audio:true
+}
+
 let init = async () => {
     try {
         client = await AgoraRTM.createInstance(APP_ID);
         await client.login({ uid, token });
         
-        channel = client.createChannel('main');
+        channel = client.createChannel(roomId);
         await channel.join();
         channel.on('MemberJoined', handleUserJoined);
+        channel.on('MemberLeft',handleUserLeft)
+
         client.on('MessageFromPeer', handleMessageFromPeer);
 
-        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        localStream = await navigator.mediaDevices.getUserMedia(constraints);
         document.getElementById('user-1').srcObject = localStream;
     } catch (error) {
         console.error('Error initializing:', error);
     }
 };
+
+let handleUserLeft = (MemberId)=>{
+    document.getElementById('user-2').style.display='none'
+    document.getElementById('user-1').classList.remove('smallFrame')
+}
 
 let handleMessageFromPeer = async (message, MemberId) => {
     try {
@@ -64,6 +87,10 @@ let createPeerConnection = async (MemberId) => {
         peerConnection = new RTCPeerConnection(servers);
         remoteStream = new MediaStream();
         document.getElementById('user-2').srcObject = remoteStream;
+        document.getElementById('user-2').style.display='block'
+        document.getElementById('user-1').classList.add('smallFrame')
+
+
         if (!localStream) {
             localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
             document.getElementById('user-1').srcObject = localStream;
@@ -127,5 +154,40 @@ let addAnswer = async (answer) => {
         console.error('Error adding answer:', error);
     }
 };
+
+let leaveChannel = async () =>{
+    await channel.leave()
+    await client.logout()
+}
+
+let toggleCamera = async () => {
+    let videoTrack=localStream.getTracks().find(track=>track.kind==='video')
+    if(videoTrack.enabled){
+        videoTrack.enabled=false
+        document.getElementById('camera-btn').style.backgroundColor='rgb(255,80,80)'
+    }
+    else{
+        videoTrack.enabled=true
+        document.getElementById('camera-btn').style.backgroundColor='green'
+    }
+}
+
+let toggleMic = async () => {
+    let audioTrack=localStream.getTracks().find(track=>track.kind==='audio')
+    if(audioTrack.enabled){
+        audioTrack.enabled=false
+        document.getElementById('mic-btn').style.backgroundColor='rgb(255,80,80)'
+    }
+    else{
+        audioTrack.enabled=true
+        document.getElementById('mic-btn').style.backgroundColor='green'
+    }
+}
+
+
+window.addEventListener('beforeunload',leaveChannel)
+
+document.getElementById('camera-btn').addEventListener('click',toggleCamera)
+document.getElementById('mic-btn').addEventListener('click',toggleMic)
 
 init();
